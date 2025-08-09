aefe9a2d59e199b1eef1955d9097a0a31e6ad3cb
Stanley <stanley.s@ihealthwellness.org>
2025-08-09 10:02:58 -0400

    Fixed mobile and tablet versions

--- src/components/layout/SecondaryBar.tsx
+++ src/components/layout/SecondaryBar.tsx
@@ -45,8 +45,7 @@ const SecondaryBar: React.FC<SecondaryBarProps> = ({
                 <Image
                   src={button.imageSrc || `/${button.type}-store-badge.png`}
                   alt={button.alt || `${button.type === 'google' ? 'Google Play' : 'App Store'} badge`}
-                  width={135}
-                  height={40}
+                  fill
                   className="h-10 w-auto"
                 />
               </a>
